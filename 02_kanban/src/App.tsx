9dad3657b0835255716197b384476ed66b65b1ef
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-07-28 16:53:39 -0500

    Add draggable pokemon.

--- 02_kanban/src/App.tsx
+++ 02_kanban/src/App.tsx
@@ -1,58 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Board from './components/Board';
 
-export interface IPokemon {
-  name: string;
-  url: string;
-  // [index: number]: {
-  //   name: string;
-  //   url: string;
-  // };
-}
-
 function App() {
-  const [pokemon, setPokemon] = useState<Array<IPokemon>>([]);
-
-  useEffect(() => {
-    const fetch10Pokemon = () => {
-      let promises = [];
-      for (let i = 1; i <= 10; i++) {
-        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`));
-      }
-      return Promise.all(promises);
-    };
-
-    const awaitJson = (responses: Response[]) => {
-      return Promise.all(
-        responses.map((response) => {
-          if (response.ok) return response.json();
-          throw new Error(response.statusText);
-        })
-      );
-    };
-
-    fetch10Pokemon()
-      .then(awaitJson)
-      .then((pokemonData: Array<any>) => {
-        const mappedPokemonData = pokemonData.map((pd) => {
-          return {
-            name: pd.name,
-            url: pd.sprites.front_default,
-          };
-        });
-        setPokemon([...mappedPokemonData]);
-      });
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  // useEffect(() => {
-  //   console.log(pokemon);
-  // }, [pokemon]);
-
   return (
     <div className="App">
       <header className="App-header">
-        <Board pokemon={pokemon}></Board>
+        <Board></Board>
       </header>
     </div>
   );
--- 02_kanban/src/components/Board.tsx
+++ 02_kanban/src/components/Board.tsx
@@ -1,14 +1,95 @@
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React, { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-import { IPokemon } from '../App';
+interface IPokemon {
+  name: string;
+  url: string;
+}
 
-export default function Board(props: { pokemon: Array<IPokemon> }) {
-  console.log(props.pokemon);
+export default function Board() {
+  const [pokemon, setPokemon] = useState<Array<IPokemon>>([]);
+
+  useEffect(() => {
+    const fetch10Pokemon = () => {
+      let promises = [];
+      for (let i = 1; i <= 5; i++) {
+        const pokemonNumber = Math.round(Math.random() * 151 + 1);
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`));
+      }
+      return Promise.all(promises);
+    };
+
+    const awaitJson = (responses: Response[]) => {
+      return Promise.all(
+        responses.map((response) => {
+          if (response.ok) return response.json();
+          throw new Error(response.statusText);
+        })
+      );
+    };
+
+    fetch10Pokemon()
+      .then(awaitJson)
+      .then((pokemonData: Array<any>) => {
+        const mappedPokemonData = pokemonData.map((pd) => {
+          return {
+            name: pd.name,
+            url: pd.sprites.front_default,
+          };
+        });
+        setPokemon(mappedPokemonData);
+      });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const dragEnded = (result: any) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+
+    const sourcePokemon: IPokemon = pokemon.find(
+      (pokemon) => pokemon.name === draggableId
+    ) as IPokemon;
+    const updatedPokemon: IPokemon[] = Array.from(pokemon);
+    updatedPokemon.splice(source.index, 1);
+    updatedPokemon.splice(destination.index, 0, sourcePokemon);
+    console.log({ updatedPokemon });
+    setPokemon(updatedPokemon);
+  };
   return (
-    <div>
-      {props.pokemon.map((prop, i) => {
-        return <div key={i}>{prop.name}</div>;
-      })}
-    </div>
+    <DragDropContext onDragEnd={dragEnded}>
+      <Droppable droppableId="first-column">
+        {(a) => (
+          <div>
+            <div ref={a.innerRef} {...a.droppableProps} className="flex flex-col space-y-4">
+              {pokemon.map((prop, i) => {
+                return (
+                  <Draggable draggableId={prop.name} index={i} key={prop.name}>
+                    {(b) => (
+                      <div
+                        className="border rounded p-2 flex place-items-center"
+                        {...b.dragHandleProps}
+                        {...b.draggableProps}
+                        ref={b.innerRef}
+                      >
+                        <div>
+                          <img src={prop.url} alt="" className="w-32" />
+                        </div>
+                        <div>{prop.name}</div>
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {a.placeholder}
+            </div>
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
