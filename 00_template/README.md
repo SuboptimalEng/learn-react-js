70194e96e65818282b77ea18d946ef8b00a60c5f
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-17 10:01:52 -0500

    Add React + TypeScript + Tailwind boilerplate.

---
+++ 00_template/README.md
@@ -0,0 +1,4 @@
+# React + TypeScript + Tailwind Template
+
+npm install
+npm run start
---
+++ 00_template/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="text-blue-400 font-black text-6xl">Learn React</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
