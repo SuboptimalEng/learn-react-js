cb3bb5af4aa344f2b4133dfc7d077042623d86a1
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-17 14:55:30 -0500

    Update styling.

--- 04_todo/src/App.tsx
+++ 04_todo/src/App.tsx
@@ -19,6 +19,16 @@ function App() {
       text: 'there',
       completed: true,
     },
+    {
+      id: Math.floor(Math.random() * 1000),
+      text: 'hello',
+      completed: true,
+    },
+    {
+      id: Math.floor(Math.random() * 1000),
+      text: 'world',
+      completed: true,
+    },
   ]);
 
   const toggleToDo = (toDoId: number) => {
@@ -31,24 +41,28 @@ function App() {
   return (
     <div className="App ">
       <header className="App-header">
-        <div className="text-4xl flex flex-col place-items-center space-y-4">
-          <div>To-Do App</div>
+        <div className="text-6xl flex flex-col place-items-center space-y-4">
           <div>
             {toDos.map((toDo) => {
               return (
                 <div key={toDo.id}>
-                  <div className="flex space-x-2 place-items-center">
-                    <div className={toDo.completed ? 'line-through' : ''}>
+                  <div className="flex space-x-8 items-end justify-between">
+                    <div
+                      className={
+                        toDo.completed ? 'line-through text-gray-400' : ''
+                      }
+                    >
                       {toDo.text}
                     </div>
+
                     {toDo.completed ? (
                       <div
-                        className="border p-2 bg-green-500 h-4 w-4 rounded"
+                        className="border mb-2 p-4 bg-green-500 rounded"
                         onClick={() => toggleToDo(toDo.id)}
                       ></div>
                     ) : (
                       <div
-                        className="border p-2 bg-white rounded"
+                        className="border mb-2 p-4 bg-white rounded"
                         onClick={() => toggleToDo(toDo.id)}
                       ></div>
                     )}
