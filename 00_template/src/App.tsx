92ef1db06e984f7d61613832b87febfff0d66c41
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-17 10:04:37 -0500

    Minor template changes.

--- 00_template/src/App.tsx
+++ 00_template/src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="text-blue-400 font-black text-6xl">Learn React</div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link text-4xl"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
