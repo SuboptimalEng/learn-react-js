fbca30a403b17abfdf4f2857cbe32a07218e9e40
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-17 16:32:39 -0500

    Add css.

--- 04_todo/src/App.tsx
+++ 04_todo/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AppWithReactHooks from './AppWithReactHooks';
+import './App.css';
 
 export interface IToDo {
   id: number;
@@ -11,7 +12,14 @@ function App() {
   const [counter, setCounter] = useState(0);
   console.log(counter, setCounter);
   // NOTE: Check how app is done with hooks
-  return <AppWithReactHooks></AppWithReactHooks>;
+  // return <AppWithReactHooks></AppWithReactHooks>;
+  return (
+    <div className="App ">
+      <header className="App-header">
+        <div>{counter}</div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
