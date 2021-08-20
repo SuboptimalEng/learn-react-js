b33c46bab65a448ae085199d5e67236a5863f0de
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-20 17:18:24 -0500

    Rename folders.

--- templates/01_vite_electron/src/background/main.js
+++ templates/01_vite_electron/src/background/main.js
@@ -5,6 +5,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    vibrancy: 'under-window',
+    visualEffectState: 'followWindow',
   });
 
   // win.loadFile('index.html');
