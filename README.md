2e8cd21df500887f2f795a6e0089836b71627c9b
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-25 11:10:29 -0500

    Remove templates.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ⚛️ LearnReact
+
 ### A project-based approach to learning React + TypeScript.
--- templates/00_basic/src/themes/dark.ts
+++
@@ -1,7 +0,0 @@
-import { createTheme } from './utils';
-
-export const darkTheme = createTheme({
-  primary: '#212529',
-  secondary: '#343A40',
-  textBase: 'white',
-});
